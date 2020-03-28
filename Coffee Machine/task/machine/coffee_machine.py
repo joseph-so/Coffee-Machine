89dc280af370a7718b1deaf0fdfad6b8083b8918
Joseph <joseph-so@gmx.net>
2020-03-28 15:03:51 +1100

    Complete Stage 4

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,24 +1,56 @@
 # Write your code here
-required_water = 200
-required_milk = 50
-required_coffee_beans = 15
+water = 1200
+milk = 540
+coffee_beans = 120
+disposable_cup = 9
+money = 550
 
-print("Write how many ml of water the coffee machine has:")
-water = int(input())
+print(f"""The coffee machine has:
+{water} of water
+{milk} of milk
+{coffee_beans} of coffee beans
+{disposable_cup} of disposable_cups
+{money} of money""")
+print()
 
-print("Write how many ml of milk the coffee machine has:")
-milk = int(input())
+coffee = [
+    {"water": 250, "milk": 0, "coffee_beans": 16, "price": 4},  # espresso
+    {"water": 350, "milk": 75, "coffee_beans": 20, "price": 7},  # latte
+    {"water": 200, "milk": 100, "coffee_beans": 12, "price": 6}  # cappuccino
+]
 
-print("Write how many grams of coffee beans the coffee machine has:")
-coffee_beans = int(input())
+print("Write action (buy, fill, take):")
+action = input()
 
-print("Write how many cups of coffee will you need:")
-cup = int(input())
+if action == "buy":
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    choice = int(input())
+    water -= coffee[choice - 1]["water"]
+    milk -= coffee[choice - 1]["milk"]
+    coffee_beans -= coffee[choice - 1]["coffee_beans"]
+    disposable_cup -= 1
+    money += coffee[choice - 1]["price"]
+elif action == "fill":
+    print("Write how many ml of water do you want to add:")
+    fill = int(input())
+    water += fill
+    print("Write how many ml of milk do you want to add:")
+    fill = int(input())
+    milk += fill
+    print("Write how many grams of coffee beans do you wants to add:")
+    fill = int(input())
+    coffee_beans += fill
+    print("Write how many disposable cups of coffee do you want to add:")
+    fill = int(input())
+    disposable_cup += fill
+elif action == "take":
+    print(f"I gave you ${money}")
+    money = 0
 
-no_of_cup = min(water // required_water, milk // required_milk, coffee_beans // required_coffee_beans)
-
-if no_of_cup < cup:
-    print(f"No, I can make only {no_of_cup} cups of coffee")
-else:
-    print("Yes, I can make that amount of coffee", end='')
-    print(f" (and even {no_of_cup - cup} more than that)" if no_of_cup > cup else "")
+print()
+print(f"""The coffee machine has:
+{water} of water
+{milk} of milk
+{coffee_beans} of coffee beans
+{disposable_cup} of disposable_cups
+{money} of money""")
